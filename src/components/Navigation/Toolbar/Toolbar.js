1c53a17ccdb8cd0af0c9ba9875e2c79d02bebc00
carlos-novaes <carlosnovaes@poli.ufrj.br>
2019-05-01 16:43:40 -0300

    making responsive adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ const Toolbar = props => {
   return (
     <header className={styles.Toolbar}>
       <div>MENU</div>
-      <Logo />
+      <div className={styles.Logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
