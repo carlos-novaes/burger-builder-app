076981758b9c65494b56d3b3b172425f674c2fa8
carlos-novaes <carlosnovaes@poli.ufrj.br>
2019-05-02 21:58:28 -0300

    reusing the backdrop in responsive view

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => {
-  return (
-    <Fragment>
-      <Toolbar />
-      <SideDrawer />
-      <main className={styles.Content}>{props.children}</main>
-    </Fragment>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
+  render() {
+    return (
+      <Fragment>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
 export default Layout;
